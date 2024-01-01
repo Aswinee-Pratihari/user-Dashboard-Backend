a5cfc5180158858cdd67fbb0cd409541a65992e9
Aswinee-Pratihari <aswineepratihari010@gmail.com>
2024-01-02 00:12:18 +0530

    Contact route created

--- models/ContactModel.js
+++ models/ContactModel.js
@@ -1,28 +1,31 @@
 import mongoose from "mongoose";
 
-const ContactSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: [true, "name is required."],
+const ContactSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "name is required."],
+    },
+    email: {
+      type: String,
+      required: [true, "email is required."],
+    },
+    phone: {
+      type: String,
+      required: [true, "phone number is required."],
+      match: [
+        /^[0-9]+$/,
+        "Please enter a valid phone number with only numeric characters.",
+      ],
+      minlength: [7, "The minimum length should be 7"],
+    },
+    postedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
   },
-  email: {
-    type: String,
-    required: [true, "email is required."],
-  },
-  phone: {
-    type: String,
-    required: [true, "phone number is required."],
-    match: [
-      /^[0-9]+$/,
-      "Please enter a valid phone number with only numeric characters.",
-    ],
-    minlength: [7, "The minimum length should be 7"],
-  },
-  postedBy: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-  },
-});
+  { timestamps: true }
+);
 
 const Contact = new mongoose.model("Contact", ContactSchema);
 export default Contact;
--- routes/contact.js
+++ routes/contact.js
@@ -29,17 +29,62 @@ router.post("/", TokenVerification, async (req, res) => {
 
 router.get("/", TokenVerification, async (req, res) => {
   try {
-    const myContacts = await Contact.find({ postedBy: req.user._id }).populate(
-      "postedBy",
-      "-password"
-    );
+    const searchTerm = req.query.query;
+    const sort = req.query.sort;
 
-    return res.status(200).json({ contacts: myContacts.reverse() });
+    const sortOptions = {};
+
+    switch (sort) {
+      case "AZ":
+        sortOptions.username = 1;
+        break;
+      case "ZA":
+        sortOptions.username = -1;
+        break;
+      case "LastModified":
+        sortOptions.updatedAt = -1;
+        break;
+      case "LastInserted":
+        sortOptions.createdAt = 1;
+        break;
+      default:
+        sortOptions.createdAt = 1;
+        break;
+    }
+
+    const myContacts = await Contact.find({
+      postedBy: req.user._id,
+      $or: [
+        { username: { $regex: searchTerm, $options: "i" } },
+        { email: { $regex: searchTerm, $options: "i" } },
+        { phone: { $regex: searchTerm, $options: "i" } },
+      ],
+    })
+      .sort(sortOptions)
+      .populate("postedBy", "-password");
+
+    return res.status(200).json({ contacts: myContacts });
   } catch (err) {
     console.log(err);
   }
 });
 
+router.get("/:id", TokenVerification, async (req, res) => {
+  const { id } = req.params;
+
+  if (!id) return res.status(400).json({ error: "no id specified." });
+
+  if (!mongoose.isValidObjectId(id))
+    return res.status(400).json({ error: "please enter a valid id" });
+
+  try {
+    const contact = await Contact.findOne({ _id: id });
+
+    return res.status(200).json({ ...contact._doc });
+  } catch (err) {
+    console.log(err);
+  }
+});
 router.put("/:id", TokenVerification, async (req, res) => {
   const { id } = req.params;
   if (!id) return res.status(400).json({ error: "no id specified." });
