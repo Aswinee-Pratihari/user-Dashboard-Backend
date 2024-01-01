7139d206ccd3bd3ed9b6abc10ddf33d7039c0260
Aswinee-Pratihari <aswineepratihari010@gmail.com>
2024-01-01 12:32:00 +0530

    Login feature completed

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,10 @@
 import express from "express";
 import User from "../models/UserModel.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 const router = express.Router();
+
 router.post("/register", async (req, res) => {
   const { name, email, password, phone, gender, heardAbout, city, state } =
     req.body;
@@ -52,6 +55,40 @@ router.post("/register", async (req, res) => {
     result._doc.password = undefined;
 
     return res.status(201).json({ ...result._doc });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: err.message });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password)
+    return res
+      .status(400)
+      .json({ error: "please enter all the required fields!" });
+
+  try {
+    const doesUserExits = await User.findOne({ email });
+
+    if (!doesUserExits)
+      return res.status(400).json({ error: "Invalid email or password!" });
+
+    // if there were any user present.
+    const doesPasswordMatch = await bcrypt.compare(
+      password,
+      doesUserExits.password
+    );
+
+    if (!doesPasswordMatch)
+      return res.status(400).json({ error: "Invalid email or password!" });
+
+    const payload = { _id: doesUserExits._id };
+    const token = jwt.sign(payload, process.env.JWT_SECRET);
+
+    const user = { ...doesUserExits._doc, password: undefined };
+    return res.status(200).json({ token, user });
   } catch (err) {
     console.log(err);
     return res.status(500).json({ error: err.message });
